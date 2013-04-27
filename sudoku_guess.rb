1f516378db25fc37fcb8611e826369ce0f924ba1
dbc-apprentice <apprentice@devbootcamp.com>
2013-04-26 19:50:03 -0500

    Second try

--- sudoku_guess.rb
+++ sudoku_guess.rb
@@ -1,41 +1,41 @@
 # NOT COMPLETE
-
-
+ 
+ 
 require 'pry'
 require 'debugger'
 class Sudoku
   
   attr_reader :board
-
+ 
   def initialize(board_string)
     @board = Board.new(board_string)
   end
-
+ 
   def solve(test_board = @board.clone)
-    puts test_board
+
+    puts test_board if test_board.solved?
+    abort("We win!") if test_board.solved?
     x = 0
-
+ 
     y = test_board.board_map.find_index do |row| 
-
+ 
       x = row.find_index do |cell|
         cell == 0
       end
     end
-
+ 
     possibles = possible_values(x, y, test_board)
-    
-
     possibles.each do |test_case|
       test_board_clone = test_board.clone
       test_board_clone.board_map[y][x] = test_case
-      return solve(test_board_clone) if solve(test_board_clone)
-       
-    end
-    return test_board if test_board.solved?
+      return test_board_clone if solve(test_board_clone)
+      end
+    return test_board_clone if test_board.solved?
   end
-
+ 
   
   def possible_values(x_coord, y_coord, test_board)
+    return nil if test_board.solved?
     possibles =[]
     (1..9).each do |number|
       possibles << number if not_in_row?(number, y_coord,test_board) \
@@ -44,15 +44,15 @@ class Sudoku
     end
     return possibles
   end
-
+ 
   def not_in_row?(number, row, test_board)
-    !test_board.board_map[row].include?(number)
+      !test_board.board_map[row].include?(number)
   end
-
+ 
   def not_in_column?(number, column, test_board)
     test_board.board_map.none? { |row| row[column] == number }
   end
-
+ 
   def not_in_box?(number, x, y, test_board)
     starting_x = (x/3) * 3 
     starting_y = (y/3) * 3
@@ -62,9 +62,9 @@ class Sudoku
       end
     end
   end
-
+ 
 end
-
+ 
 class Board
   attr_accessor :board_map
   
@@ -72,7 +72,7 @@ class Board
     @board_map = board_string.split('').each_slice(9).to_a
     @board_map.map! { |row| row.map(&:to_i) }
   end
-
+ 
   def to_s
     puts ''
     @board_map.each do |row|
@@ -84,19 +84,19 @@ class Board
   def solved?
     @board_map.flatten.none? {|cell| cell == 0}
   end
-
+ 
   def clone
     board_string = @board_map.flatten.join
     Board.new(board_string)
   end
 end
-
+ 
 board_string = File.readlines('sample.unsolved.txt')[1].chomp
-
+ 
 game = Sudoku.new(board_string)
-
-
+ 
+ 
 # Remember: this will just fill out what it can and not "guess"
 p game.solve
-
+ 
 puts game.board
